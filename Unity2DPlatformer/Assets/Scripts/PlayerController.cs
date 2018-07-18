645bac220b0833a07d8947a191b0b6da351f0714
MSI\Dan <dan.nunley@gmail.com>
2018-07-17 22:22:51 -0400

    player movement scripts added

--- Unity2DPlatformer/Assets/Scripts/PlayerController.cs
+++ Unity2DPlatformer/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public float moveSpeed;
+	public float jumpSpeed;
+	private Rigidbody2D myRigidbody;
+
 	// Use this for initialization
 	void Start () {
-		
+		myRigidbody = GetComponent<Rigidbody2D>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetAxisRaw("Horizontal") > 0f){
+			myRigidbody.velocity = new Vector3(moveSpeed,myRigidbody.velocity.y,0f);
+		}else if(Input.GetAxisRaw("Horizontal") < 0f){
+			myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y,0f);
+		}else{
+			myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
+		}
+
+		if(Input.GetButtonDown("Jump")){
+			myRigidbody.velocity = new Vector3(myRigidbody.velocity.x,jumpSpeed,0f);
+		}
 	}
 }
