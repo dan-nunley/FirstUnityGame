43b79683f92c73732c5c080aa61565fb9bca3e07
MSI\Dan <dan.nunley@gmail.com>
2018-07-25 21:22:17 -0400

    Added player movement

--- Unity2DPlatformer/Assets/Scripts/PlayerController.cs
+++ Unity2DPlatformer/Assets/Scripts/PlayerController.cs
@@ -6,26 +6,45 @@ public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
 	public float jumpSpeed;
+
 	private Rigidbody2D myRigidbody;
+
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+
+	public int doubleJumpCount;
+	public bool isGrounded;
+
+	private Animator myAnimator;
 
 	// Use this for initialization
 	void Start () {
 		myRigidbody = GetComponent<Rigidbody2D>();
+		myAnimator = GetComponent<Animator>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
 		if(Input.GetAxisRaw("Horizontal") > 0f){
-			myRigidbody.velocity = new Vector3(moveSpeed,myRigidbody.velocity.y,0f);
+			myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+			transform.localScale = new Vector3(1f, 1f, 1f);
 		}else if(Input.GetAxisRaw("Horizontal") < 0f){
-			myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y,0f);
+			myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+			transform.localScale = new Vector3(-1f, 1f, 1f);
 		}else{
 			myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
 		}
 
-		if(Input.GetButtonDown("Jump")){
-			myRigidbody.velocity = new Vector3(myRigidbody.velocity.x,jumpSpeed,0f);
+		if(Input.GetButtonDown("Jump") && (isGrounded || (doubleJumpCount%2 == 0))){
+			myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
+			doubleJumpCount++;
 		}
+
+		myAnimator.SetFloat("Speed", Mathf.Abs(myRigidbody.velocity.x));
+		myAnimator.SetBool("Grounded", isGrounded);
 	}
 }
